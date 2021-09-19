b65968150f9fe0221b2b3769384541a86bb674f6
Abdul Aziz Barkat <abdul_barkat@persistent.com>
2021-09-19 14:38:41 +0530

    Configure database

--- library-management-system/src/main/java/com/psl/gems/LibraryManagementSystemApplication.java
+++ library-management-system/src/main/java/com/psl/gems/LibraryManagementSystemApplication.java
@@ -1,13 +1,37 @@
 package com.psl.gems;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.psl.gems.dao.UserRepository;
+import com.psl.gems.model.User;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
+	
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+	
+	@Bean
+	InitializingBean initialCode() {
+		return () -> {
+			User user = new User();
+			user.setName("Admin");
+			user.setUsername("admin");
+			user.setPassword("user");
+			user.setRole("librarian");
+			userRepository.save(user);
+			for (User usr : userRepository.findAll()) {
+				System.out.println(usr);
+			}
+		};
 	}
 
 }
--- library-management-system/src/main/java/com/psl/gems/model/User.java
+++ library-management-system/src/main/java/com/psl/gems/model/User.java
@@ -1,12 +1,15 @@
 package com.psl.gems.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 		
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int user_id;
 	String name;
 	String role;
