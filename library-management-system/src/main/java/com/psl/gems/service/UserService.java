cb893a6901f5860d9d7caccfaa910a82fa960dde
meetmandhanemnm <meetmandhanemnm@gmail.com>
2021-09-21 15:11:03 +0530

    EmployeeController added

--- library-management-system/src/main/java/com/psl/gems/service/UserService.java
+++ library-management-system/src/main/java/com/psl/gems/service/UserService.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.psl.gems.dao.UserRepository;
 import com.psl.gems.model.User;
 
+@Service
 public class UserService 
 {
 	@Autowired
