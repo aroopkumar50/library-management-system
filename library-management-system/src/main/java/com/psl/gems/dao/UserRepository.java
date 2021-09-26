37242303a8e4abe043e0c93705acec4676cd1c16
meetmandhanemnm <meetmandhanemnm@gmail.com>
2021-09-26 13:38:10 +0530

    BookService Test Cases Completed

--- library-management-system/src/main/java/com/psl/gems/dao/UserRepository.java
+++ library-management-system/src/main/java/com/psl/gems/dao/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.psl.gems.model.User;
 
