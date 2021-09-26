8d2050ea843174bdcbcf64e6bee6b78073ea2192
Meet Mandhane <51969609+meetmandhanemnm@users.noreply.github.com>
2021-09-26 13:40:18 +0530

    Update UserRepository.java

--- library-management-system/src/main/java/com/psl/gems/dao/UserRepository.java
+++ library-management-system/src/main/java/com/psl/gems/dao/UserRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.psl.gems.model.User;
 
